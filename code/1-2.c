cb718d766e251bfadba3f7a8122f07f39fa34f7c
Kanyon05 <windtree01@gmail.com>
2026-05-15 12:07:01 +0900

    Update 1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef int (*BinOp(int, int));
+
+int add (int a, int b) {
+    return a + b;
+}
+
+int multiply (int a, int b) {
+    return a * b;
+}
+
+int main() {
+    BinOp ops[2] = {add, multiply};
+
+for (int i= 0; i < 2; i++) {
+    printf("%d\n", ops[i](3, 4));
+}
+
+    return 0;
+}
