5ee28c6bd961e7c737342e4b62af53caf42c2cf9
Kanyon05 <windtree01@gmail.com>
2026-05-15 21:47:42 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -2,21 +2,20 @@
 
 typedef int (*BinOp)(int, int);
 
-int add (int a, int b) {
+int add(int a, int b) {
     return a + b;
 }
 
-int mul (int a, int b) {
+int multiply(int a, int b) {
     return a * b;
 }
 
 int main() {
-    int (*ops[2])(int, int) = {add, mul};
-    BinOp ops[2] = {add, mul};
+    BinOp ops[2] = {add, multiply};
 
-for (int i= 0; i < 2; i++) {
-    printf("%d\n", ops[i](3, 4));
-}
+    for (int i = 0; i < 2; i++) {
+        printf("%d\n", ops[i](3, 4));
+    }
 
     return 0;
 }
